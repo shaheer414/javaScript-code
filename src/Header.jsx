0aa0c243c99b2d1cbe003a0401a68b55d7433a37
Mirza Shaheer Ahmad <shaheera415@gmail.com>
2025-08-28 19:25:53 +0500

    Add files via upload

---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+        <div className="logo">Facebook</div>
+        <div className='home'>Home</div>
+        <div className='home'>About</div>
+        <div className='home'>Contact</div>
+    </div>
+);
+};
+
+export default Header;

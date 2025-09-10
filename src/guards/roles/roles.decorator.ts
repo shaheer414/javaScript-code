4e62ef461a2fe2a375680bea8b2d1d959c24b914
Mirza Shaheer Ahmad <shaheera415@gmail.com>
2025-09-10 19:35:11 +0500

    Delete src directory

--- src/guards/roles/roles.decorator.ts
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line prettier/prettier
-import { SetMetadata } from "@nestjs/common";
-
-export const ROLES_KEY = 'roles';
-
-// eslint-disable-next-line prettier/prettier
-export const Roles = (...roles :string []) => SetMetadata(ROLES_KEY, roles);
